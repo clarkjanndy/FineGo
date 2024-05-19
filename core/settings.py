ce6186d487b698121ba9595f75cc667ed99aed15
clarkjanndy <cjcasol22@gmail.com>
2024-05-19 11:18:53 +0800

    add mysql connection

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'finego',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',  # Set to 'localhost' or '127.0.0.1' if the database is on the same machine
+    #     'PORT': '3306'
+    # }
 }
 
 
